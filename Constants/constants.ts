20c11882ee1dec90ecb09a9bbae9226128573d82
Mariusz <mariusz.pankiewicz@nist.de>
2020-01-23 17:58:11 +0100

    fetch url method

--- Constants/constants.ts
+++ Constants/constants.ts
@@ -2,5 +2,5 @@ import { Dish, Location } from '../Models/models';
 
 export const defaultLocation: Location = { lat: 52.520008, lng: 13.404954} 
 export const FilterCriteria: Dish[] = ['sushi', 'pizza', 'burger']
-export const FetchUrl: ((query: string, location:Location) => string) = (query: string, location:Location) => `https://api.yelp.com/v3/businesses/search?term=$${query}&latitude=${location.lat}&longitude=${location.lng}`;
+export const FetchUrl: ((query: Dish, location:Location) => string) = (query, location) => `https://api.yelp.com/v3/businesses/search?term=$${query}&latitude=${location.lat}&longitude=${location.lng}`;
 

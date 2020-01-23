c616189d07db4ec6faa88cba2581ea1d4c2a186b
Mariusz <mariusz.pankiewicz@nist.de>
2020-01-23 08:45:07 +0100

    Text check

--- Screens/HomeScreen.tsx
+++ Screens/HomeScreen.tsx
@@ -1,6 +1,5 @@
 import React, { FunctionComponent } from 'react'
 import { View, Text, ScrollView } from 'react-native'
-// import HomeComponent from '../../Components/Home/HomeComponent'
 import HomeComponent from '../Components/HomeComponent'
 import {DataProvider} from '../Contexts/fetch.context'
 import CriteriumSelector from '../Components/CriteriumSelectorComponent'

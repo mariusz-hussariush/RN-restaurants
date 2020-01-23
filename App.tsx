2e5e56c627f33993e4870408c88a51ca39039fb8
Mariusz <mariusz.pankiewicz@nist.de>
2020-01-23 19:27:11 +0100

    nativebase elements

--- App.tsx
+++ App.tsx
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState, Fragment } from 'react';
 import { StyleSheet, View, ViewStyle } from 'react-native';
 import HomeScreen from './Screens/HomeScreen';
+import * as Font from 'expo-font';
+import { Ionicons } from '@expo/vector-icons';
+import * as Expo from "expo";
 
 interface Styles {
   container: ViewStyle
 }
 
 export default function App() {
-  
+  const [isReady, setIsReady] = useState<boolean>(false)
+
+  useEffect(() => {
+    Font.loadAsync({
+      Roboto: require('native-base/Fonts/Roboto.ttf'),
+      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+      ...Ionicons.font,
+    }).then(()=> {console.log('fonts success'); setIsReady(true)}).catch(error => console.log(error))
+  }, [])
   return (
-    <View style={styles.container}>
-      <HomeScreen/>
-    </View>
-  );
+    <Fragment>
+      {isReady ?
+       <View style={styles.container}>
+        <HomeScreen />
+      </View> :
+      <Expo.AppLoading />}
+    </Fragment>
+  )
 }
 
 const styles = StyleSheet.create<Styles>({
--- Components/HomeComponent.tsx
+++ Components/HomeComponent.tsx
@@ -1,19 +1,20 @@
 import React, { FunctionComponent } from 'react'
-import { View, FlatList, ActivityIndicator } from 'react-native'
+import { FlatList, ActivityIndicator } from 'react-native'
 import { useData } from '../Contexts/data.context'
 import FlatListItemComponent from './FlatListItemComponent';
+import { Container } from 'native-base';
 
 const HomeComponent: FunctionComponent = () => {
     const { restaurants } = useData();
     return (
-        <View>
+        <Container>
             {
                 restaurants ? <FlatList data={restaurants.businesses.slice(0, 10)} renderItem={({ item }: any) => {
                     return <FlatListItemComponent restaurant={item} />
                 }} /> :
                     <ActivityIndicator style={{ position: 'relative', top: '40%' }} size="large" color="#19596b" />
             }
-        </View>
+        </Container>
     )
 
 }
